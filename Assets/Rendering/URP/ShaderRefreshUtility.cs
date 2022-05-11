2745f1d54dcfbccb26b397a3f8cd9f0735697def
Jorian Baeke <jorian@rhinox.training>
2022-05-11 14:58:05 +0200

    ShaderRefreshUtility - SetMaterialKeywords

---
+++ Assets/Rendering/URP/ShaderRefreshUtility.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public static class ShaderRefreshUtility
+{
+    public static void SetMaterialKeywords(Material material)
+    {
+        switch (material.shader.name)
+        {
+            // TODO check name and apply the methods found in their GUI
+            default:
+                SetMaterialKeywords(material, null, null);
+                break;
+        }
+    }
+    
+    public static void SetMaterialKeywords(Material material, Action<Material> shadingModelFunc, Action<Material> shaderFunc)
+    {
+        // Clear all keywords for fresh start
+        material.shaderKeywords = null;
+
+        // Setup blending - consistent across all Universal RP shaders
+        SetupMaterialBlendMode(material);
+
+        // Receive Shadows
+        if(material.HasProperty("_ReceiveShadows"))
+            CoreUtils.SetKeyword(material, "_RECEIVE_SHADOWS_OFF", material.GetFloat("_ReceiveShadows") == 0.0f);
+
+        // Emission
+        if (material.HasProperty("_EmissionColor"))
+            MaterialEditor.FixupEmissiveFlag(material);
+        bool shouldEmissionBeEnabled =
+            (material.globalIlluminationFlags & MaterialGlobalIlluminationFlags.EmissiveIsBlack) == 0;
+        if (material.HasProperty("_EmissionEnabled") && !shouldEmissionBeEnabled)
+            shouldEmissionBeEnabled = material.GetFloat("_EmissionEnabled") >= 0.5f;
+        CoreUtils.SetKeyword(material, "_EMISSION", shouldEmissionBeEnabled);
+
+        // Normal Map
+        if (material.HasProperty("_BumpMap"))
+            CoreUtils.SetKeyword(material, "_NORMALMAP", material.GetTexture("_BumpMap"));
+
+        // Shader specific keyword functions
+        shadingModelFunc?.Invoke(material);
+        shaderFunc?.Invoke(material);
+    }
+
+    public static void SetupMaterialBlendMode(Material material)
+    {
+        if (material == null)
+            throw new ArgumentNullException("material");
+
+        bool alphaClip = false;
+        if (material.HasProperty("_AlphaClip"))
+            alphaClip = material.GetFloat("_AlphaClip") >= 0.5;
+
+        if (alphaClip)
+        {
+            material.EnableKeyword("_ALPHATEST_ON");
+        }
+        else
+        {
+            material.DisableKeyword("_ALPHATEST_ON");
+        }
+
+        if (material.HasProperty("_Surface"))
+        {
+            float surfaceType = material.GetFloat("_Surface");
+            if (surfaceType == 0)
+            {
+                if (alphaClip)
+                {
+                    material.renderQueue = (int)RenderQueue.AlphaTest;
+                    material.SetOverrideTag("RenderType", "TransparentCutout");
+                }
+                else
+                {
+                    material.renderQueue = (int)RenderQueue.Geometry;
+                    material.SetOverrideTag("RenderType", "Opaque");
+                }
+
+                material.renderQueue += material.HasProperty("_QueueOffset") ? (int)material.GetFloat("_QueueOffset") : 0;
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.SetShaderPassEnabled("ShadowCaster", true);
+            }
+            else
+            {
+                float blendMode = material.GetFloat("_Blend");
+
+                // Specific Transparent Mode Settings
+                switch (blendMode)
+                {
+                    case 0: // BlendMode.Alpha:
+                        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        break;
+                    case 1: // BlendMode.Premultiply:
+                        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                        material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                        break;
+                    case 2: // BlendMode.Additive:
+                        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        break;
+                    case 3: // BlendMode.Multiply:
+                        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.DstColor);
+                        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                        material.EnableKeyword("_ALPHAMODULATE_ON");
+                        break;
+                }
+
+                // General Transparent Material Settings
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_ZWrite", 0);
+                material.renderQueue = (int)RenderQueue.Transparent;
+                material.renderQueue += material.HasProperty("_QueueOffset") ? (int)material.GetFloat("_QueueOffset") : 0;
+                material.SetShaderPassEnabled("ShadowCaster", false);
+            }
+        }
+    }
+}
